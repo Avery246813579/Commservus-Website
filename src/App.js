41143cb4a670201da9e05b59efb150f445f08fdc
Catislin <caitlin.estrellita@gmail.com>
2017-10-08 15:26:18 -0700

    added sign up popup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class Popup extends React.Component {
+class LoginPopup extends React.Component {
   render() {
     return (
       <div className='popup'>
@@ -21,18 +21,45 @@ class Popup extends React.Component {
   }
 }
 
+class SignUpPopup extends React.Component {
+  render() {
+    return (
+      <div className='popup'>
+        <div className='popup_inner'>
+          <h1>{this.props.text}</h1>
+          <form className="Login-Form">
+            <input name="email" type="text" placeholder="Email Address" />
+            <input name="username" type="password" placeholder="Password" />
+            <div className="cancel">
+              <button type="submit"> Sign Up </button>
+              <button onClick={this.props.closePopup}>Cancel</button>
+            </div>
+            </form>
+        </div>
+      </div>
+    );
+  }
+}
+
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      showPopup: false
+      showLogin: false,
+      showSignUp: false
     };
   }
 
-  togglePopup() {
+  toggleLogin() {
     this.setState({
-      showPopup: !this.state.showPopup
+      showLogin: !this.state.showLogin
+    });
+  }
+
+  toggleSignUp() {
+    this.setState({
+      showSignUp: !this.state.showSignUp
     });
   }
 
@@ -40,17 +67,24 @@ class App extends Component {
     return (
       <div className="App">
       <div className="Login-Bar">
-        <button className="Login-Buttons" onClick={this.togglePopup.bind(this)}> Login </button>
-        <button className="Login-Buttons" onClick={this.togglePopup.bind(this)}> Sign up </button>
+        <button className="Login-Buttons" onClick={this.toggleLogin.bind(this)}> Login </button>
+        <button className="Login-Buttons" onClick={this.toggleSignUp.bind(this)}> Sign up </button>
       </div>
         <header className="App-header">
           <h1 className="App-title">Commservus</h1>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In diam velit, dapibus id maximus ac, rhoncus vel elit.  </p>
         </header>
-        {this.state.showPopup ?
-          <Popup
+        {this.state.showLogin ?
+          <LoginPopup
             text='Login'
-            closePopup={this.togglePopup.bind(this)}
+            closePopup={this.toggleLogin.bind(this)}
+          />
+          : null
+        }
+        {this.state.showSignUp ?
+          <SignUpPopup
+            text='Sign Up'
+            closePopup={this.toggleSignUp.bind(this)}
           />
           : null
         }
