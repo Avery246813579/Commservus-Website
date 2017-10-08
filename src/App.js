da1d4ffec8f5c9cb0237deee2b75df384a49cbde
Catislin <caitlin.estrellita@gmail.com>
2017-10-08 15:13:23 -0700

    added login popup window

--- src/App.js
+++ src/App.js
@@ -1,14 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+class Popup extends React.Component {
+  render() {
+    return (
+      <div className='popup'>
+        <div className='popup_inner'>
+          <h1>{this.props.text}</h1>
+          <form className="Login-Form">
+            <input name="email" type="text" placeholder="Email Address" />
+            <input name="username" type="password" placeholder="Password" />
+            <div className="cancel">
+              <button type="submit"> Login </button>
+              <button onClick={this.props.closePopup}>Cancel</button>
+            </div>
+            </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showPopup: false
+    };
+  }
+
+  togglePopup() {
+    this.setState({
+      showPopup: !this.state.showPopup
+    });
+  }
+
   render() {
     return (
       <div className="App">
+      <div className="Login-Bar">
+        <button className="Login-Buttons" onClick={this.togglePopup.bind(this)}> Login </button>
+        <button className="Login-Buttons" onClick={this.togglePopup.bind(this)}> Sign up </button>
+      </div>
         <header className="App-header">
           <h1 className="App-title">Commservus</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In diam velit, dapibus id maximus ac, rhoncus vel elit.  </p>
         </header>
+        {this.state.showPopup ?
+          <Popup
+            text='Login'
+            closePopup={this.togglePopup.bind(this)}
+          />
+          : null
+        }
       </div>
     );
   }
